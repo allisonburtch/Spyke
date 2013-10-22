aef0c754016e66bd888850646bd324b257544156
allisonburtch <allisonburtch@gmail.com>
2013-10-22 00:33:03 -0400

    trying to add peer.js stuff

--- app.rb
+++ app.rb
@@ -3,19 +3,22 @@ require 'securerandom'
 # require 'faye-websocket'
 
 #routes
-get '/' do
-  erb :index
-end
+# get '/' do
+#   erb :index
 
-get '/newcall' do
-	erb :newcall
-	random_url = SecureRandom.urlsafe_base64(20)
-end
+# get '/newcall' do
+# 	erb :newcall
 
-get '/about' do
-	erb :about
-end
-
-
-
+# get '/about' do
+# 	erb :about
+# 	end
+# 	end
+# end
 	# $redis = Redis.new
+
+get('/') { erb :index }
+# get('/apps') { @apps = Dumbstore::Text.apps.merge(Dumbstore::Voice.apps).values.uniq; erb :apps }
+get('/newcall') { erb :newcall }
+get('/about') { erb :about }
+
+# @random_url = SecureRandom.urlsafe_base64(20);
