a1ba4ad9860fe55839fd9d9e520809e907690ece
allisonburtch <allisonburtch@gmail.com>
2013-12-03 02:39:18 -0500

    added logic for get/post routes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //dependencies
 var routes = require('./routes');
-var user = require('./routes/user');
 var path = require('path');
 var https = require('https');
 var fs = require('fs');
@@ -14,7 +13,11 @@ var express = require('express'),
 var chatRooms = new Object();
 
 // all environments
+app.engine('.html', require('ejs').__express);
 app.set('port', process.env.PORT || 3333);
+app.set('view engine', 'html');
+
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -33,10 +36,25 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+
 app.get('/', function(req, res){
-  res.redirect('/index.html');
+  res.render('index');
 });
 
+// app.post('/chatroom/:id', function(req, res){
+//     console.log(req.params.id);
+//     // console.log(req.body.description_author);
+//     // console.log(req.body.description_tags);
+//     // console.log(req.body.description_textarea);
+//     // console.log(req.files);
+//     // res.send("Done!");
+// });
+
+app.get('/chatroom/:id', function(req, res){
+	res.render('chatroom');
+	newid = req.params.id;
+	console.log(newid);
+});
 //
 //start of questionable code
 //
@@ -44,37 +62,33 @@ app.get('/', function(req, res){
 // 	res.render('index.jade', {name: 'Joe'});
 // });
 
-app.post('/chatroom/:id', function(req, res){
-    console.log(req.body.description_title);
-    console.log(req.body.description_author);
-    console.log(req.body.description_tags);
-    console.log(req.body.description_textarea);
-    console.log(req.files);
-    res.send("Done!");
-});
-
-
-
-app.get('/chatroom/:id', function(req, res){
-	req.params.id
-
-
+// app.get('/chatroom', function(req, res){
+//   res.render('chatroom');
+//  //  function randomVidName() {
+//  //    var chars = "0123456789ABCDEFGHIJKLMNOPQRS-_=TUVWXYZabcdefghijklmnopqrstuvwxyz";
+//  //    var string_length = 20;
+//  //    var randomstring = '';
+//  //        for (var i = 0; i < string_length; i++) {
+//  //            var rnum = Math.floor(Math.random() * chars.length);
+//  //            randomstring += chars.substring(rnum, rnum + 1);
+//  //        }
+//  //        return randomstring;
+// 	// 	}
+// 	// res.send(randomVidName());
 // });
-// function randomVidName() 
-//     {
-//         var chars = "0123456789ABCDEFGHIJKLMNOPQRS-_=TUVWXYZabcdefghijklmnopqrstuvwxyz";
-//         var string_length = 20;
-//         var randomstring = '';
-//             for (var i = 0; i < string_length; i++) 
-//             {
-//                 var rnum = Math.floor(Math.random() * chars.length);
-//                 randomstring += chars.substring(rnum, rnum + 1);
-//             }
-//             return randomstring;
-// }
+
+// app.get(/^\/post\/([a-z\d\-]*)/, function (req, res, next) {
+//   // req.params contains the matches set by the RegExp capture groups
+
+//   res.render('chatroom')
+// })
+
+
+
 // app.post('/makeChatroom/:id', function(req, res){
-// // you generate a chatroom which is a 20 char code.
-// //res.redirect('/chatroom.html')
+
+// you generate a chatroom which is a 20 char code.
+// res.redirect('/chatroom.html')
 
 // //and locally on the server you have two unique ids that have been generated
 // //client 1 gets a peerid which is also the url/chatroomid
@@ -103,16 +117,16 @@ app.get('/chatroom/:id', function(req, res){
 // }
 
 // app.get('/chatRoom/:id', function(req, res){
-// // server will check req.params.id (which is what client 2 sned it) against current chatrooms that are alive
-// //it will then generate a peer id
-// //for loop of vc.chatrooms
-// //for (var i =0; i<vc.chatrooms.length; i++ ){
-// //	if vc.chatrooms[i].chatRoomURL == req.params.id
-// //YOU GIVE CLIENT2 AN ID.
-// // add a new peer to that chatroom and then do al the peerjs stuff
+// server will check req.params.id (which is what client 2 sned it) against current chatrooms that are alive
+// it will then generate a peer id
+// for loop of vc.chatrooms
+// for (var i =0; i<vc.chatrooms.length; i++ ){
+// 	if vc.chatrooms[i].chatRoomURL == req.params.id
+// YOU GIVE CLIENT2 AN ID.
+// add a new peer to that chatroom and then do al the peerjs stuff
 
-// //}
-//
+// }
+
 // }
 
 // app.listen(3333);
