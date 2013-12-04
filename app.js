3461b57e75643051bf1b144f41f6d958634fc415
allisonburtch <allisonburtch@gmail.com>
2013-12-04 00:48:11 -0500

    such docoumentation. wow codes.

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var express = require('express'),
     server = http.createServer(app), 
     io = require('socket.io');
 
-var chatRooms = new Object();
-
 // all environments
 app.engine('.html', require('ejs').__express);
 app.set('port', process.env.PORT || 3333);
@@ -31,7 +29,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
 var sockets = io.listen(server);
+var chatRooms = new Object();
 
 //for heroku
 sockets.configure(function() {
@@ -39,19 +39,56 @@ sockets.configure(function() {
   sockets.set('polling duration', 10);
 });
 
+//creating the http/socket server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+//getting the main page with the 'create chatroom' putton
 app.get('/', function(req, res){
   res.render('index');
 });
 
+//randomly generated url that gets the chatroom.html page
+//newid is everything after /chatroom/ in the url
+//this is where
 app.get('/chatroom/:id', function(req, res){
 	res.render('chatroom');
 	newid = req.params.id;
-	console.log(newid);
+	console.log("your new chatroom id is " + newid);
+
+	chatRooms = {
+	  	"chatRoomURL": chatRoomURL,
+	  	"peers": 
+	  	[
+	  	 "peerOne": peer1,
+	  	 "peerTwo": peer2,
+	  	 "peerThree": peer3,
+	  	 "peerFour": peer4
+	  	]
+	  }
+
+	for (var i = 0; i<chatRooms.length; i++){
+		chatRooms[i]['chatRoomURL']
+		if(chatRooms[i].chatRoomURL == newid){
+			push.chatRooms;
+		}
+	}
+
+
+	
 	//push newid to an array
+
+
+
+push 
+//i 
+
+	socket.on connections
+	for socket connection, if newid == new
+
+
+
 });
 
 //using sockets to tell people to open connections to eachother
@@ -61,44 +98,8 @@ app.get('/chatroom/:id', function(req, res){
 //socket.io
 //now associating 
 
-// app.post('/chatroom/:id', function(req, res){
-//     console.log(req.params.id);
-//     // console.log(req.body.description_author);
-//     // console.log(req.body.description_tags);
-//     // console.log(req.body.description_textarea);
-//     // console.log(req.files);
-//     // res.send("Done!");
-// });
-
-//start of questionable code
-//
-// app.get('/v\/(.*)/', function (req, res) {
-// 	res.render('index.jade', {name: 'Joe'});
-// });
 
 
-// app.post('/makeChatroom/:id', function(req, res){
-
-// you generate a chatroom which is a 20 char code.
-// res.redirect('/chatroom.html')
-
-// //and locally on the server you have two unique ids that have been generated
-// //client 1 gets a peerid which is also the url/chatroomid
-//  //1. is the chatroomiD which is the ur;
-//  //2. is a peerId for client 1.
-//  // and creat json object that 
-
- // var newUrl = randomVidName();
- // // for cooperate-defect package globals
- //  // set some defaults
- //  var newChatRoom = {
- //  	"chatRoomURL": newUrl,
- //  	 "peerOne": 
- //  	 "peerTwo":
- //  	 "peerThree":
- //  	 "peerFour":
- //  	]
- //  }	
 
  //  chatRooms.push(newChatroom);
 
@@ -116,9 +117,21 @@ app.get('/chatroom/:id', function(req, res){
 // YOU GIVE CLIENT2 AN ID.
 // add a new peer to that chatroom and then do al the peerjs stuff
 
-// }
 
-// }
+// app.post('/chatroom/:id', function(req, res){
+//     console.log(req.params.id);
+//     // console.log(req.body.description_author);
+//     // console.log(req.body.description_tags);
+//     // console.log(req.body.description_textarea);
+//     // console.log(req.files);
+//     // res.send("Done!");
+// });
 
-// app.listen(3333);
+
+
+// //and locally on the server you have two unique ids that have been generated
+// //client 1 gets a peerid which is also the url/chatroomid
+//  //1. is the chatroomiD which is the ur;
+//  //2. is a peerId for client 1.
+//  // and creat json object that 
 
