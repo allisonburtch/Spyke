e165f581563db6f3d153222a24215cf906d81048
allisonburtch <allisonburtch@gmail.com>
2013-12-14 23:54:17 -0500

    cleaned up code

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //dependencies
 var routes = require('./routes');
+var express = require('express');
 var path = require('path');
 var https = require('https');
 var fs = require('fs');
 var url = require('url');
-var express = require('express'),
-    app = express(), 
+var app = express(), 
     http = require('http'),
     server = http.createServer(app), 
     io = require('socket.io');
@@ -27,30 +27,20 @@ app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
 
-// development only
-if ('development' == app.get('env')) {
+app.configure('development', function(){
   app.use(express.errorHandler());
-}
-
-var sockets = io.listen(server);
-console.log("server info " + sockets);
-
-var chatRooms = new Object();
-
-//for heroku
-sockets.configure(function() {
-  sockets.set('transports', ['xhr-polling']);
-  sockets.set('polling duration', 10);
 });
-
 
 //creating the http/socket server
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+var sockets = io.listen(server);
+console.log("server info " + sockets);
 
-//getting the main page with the 'create chatroom' putton
+var chatRooms = new Object();
+
 app.get('/', function(req, res){
   res.render('index');
 });
@@ -66,9 +56,7 @@ app.get('/chatroom/:id', function(req, res){
 
 sockets.on('connection', function (socket) {
 	console.log("We have a new client: " + socket.id);
-    // socket.emit('message', { message: 'welcome to the chat' });
-    // socket.on('event', function(data){});
-
+  
 	socket.on('peer', function(data) {
 		peerID = data.peer_id;
 		chatRoomURL = data.chatroom;
@@ -83,7 +71,44 @@ sockets.on('connection', function (socket) {
 		for (var i  = 0; i < sockets.length; i++) {
 			console.log("loop: " + i + " " + sockets[i].peer);
 		}
+
+		if (newid == data.chatroom)
+		{
+		
+			socket.broadcast.emit('peer_id', data);
+			console.log('data: ', data);
+		}
+	});
+
+	socket.on('newPeer', function(data){
+		newPeer = data;
+		console.log("this is the second peer id " + newPeer);
+		socket.broadcast.emit('newPeer', data);
+		});
+
+	socket.on('error', function (data) {
+		error = data;
+	    console.log("there's an error " + error);
+		});
+
+	socket.on('disconnect', function() {
+        	console.log("Client has disconnected");
+        });
+});
+
+
+
+
+
+//for heroku
+// sockets.configure(function() {
+//   sockets.set('transports', ['xhr-polling']);
+//   sockets.set('polling duration', 10);
+// });
+
+//getting the main page with the 'create chatroom' putton
 	
+	//i think i might need to push to an array but who knows
 		// chatRooms = {
 	 //  	"chatRoomURL": chatRoomURL,
 		//   	"peerID" : data.peer_id,
@@ -95,42 +120,7 @@ sockets.on('connection', function (socket) {
 		// 		push.chatRooms;
 		// 	}
 		// }
-		if (newid == data.chatroom)
-		{
-		
-			socket.broadcast.emit('peer_id', data);
-			console.log('data: ', data);
-		}
-		//send chatroom id,
-		//receive chatroom
-		// and send chatroom id
-		// Tell everyone my peer_id
-		
-	});
 
-	socket.on('newPeer', function(data){
-		newPeer = data;
-		console.log("this is the second peer id " + newPeer);
-		socket.broadcast.emit('newPeer', data);
-		});
-
-  	socket.on('disconnect', function(){});
-
-	socket.on('error', function (data) {
-		error = data;
-	    console.log("there's an error " + error);
-	});
-});
-
-
-
-//  		socket.on('otherevent', function(data) {
-// 			console.log("Received: 'otherevent' " + data);
-//                 });
-
-//         socket.on('disconnect', function() {
-//         	console.log("Client has disconnected");
-//         });
 	
 	// "peers": 
 	//   	[
@@ -140,57 +130,3 @@ sockets.on('connection', function (socket) {
 	//   	 "peerFour": peer4
 	//   	]
 // });
-// 	//push newid to an array
-//  });
-
-
-// // //separate socket.io from express stuff 
-
-// app....
-// socket..on
-
-// push 
-// //i 
-
-// 	socket.on connections
-// 	for socket connection, if newid == new
-
-
-
-// });
-
-//using sockets to tell people to open connections to eachother
-
-// socket server in this room and has x peer id
-// if people have same room id send eachother peer id 
-//socket.io
-//now associating 
-
-
-
-
- //  chatRooms.push(newChatroom);
-
-// 	app.redirect("/chatRoom.html");
-//   //https://github.com/karlward/cooperate-defect/blob/master/server/src/server.js
-
-// }
-
-// app.get('/chatRoom/:id', function(req, res){
-// server will check req.params.id (which is what client 2 sned it) against current chatrooms that are alive
-// it will then generate a peer id
-// for loop of vc.chatrooms
-// for (var i =0; i<vc.chatrooms.length; i++ ){
-// 	if vc.chatrooms[i].chatRoomURL == req.params.id
-// YOU GIVE CLIENT2 AN ID.
-// add a new peer to that chatroom and then do al the peerjs stuff
-
-
-
-
-// //and locally on the server you have two unique ids that have been generated
-// //client 1 gets a peerid which is also the url/chatroomid
-//  //1. is the chatroomiD which is the ur;
-//  //2. is a peerId for client 1.
-//  // and creat json object that 
-
